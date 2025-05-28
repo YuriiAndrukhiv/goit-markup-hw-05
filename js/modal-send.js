1c0fd063587e05705eb4f6f630d1e3177a769e81
YuiiAndrukhiv <yurii.andrukhiv@gmail.com>
2025-05-28 16:52:18 +0300

    .

--- js/modal-send.js
+++ js/modal-send.js
@@ -4,10 +4,24 @@
           .addEventListener('submit', e => {
             e.preventDefault();
 
-            new FormData(e.currentTarget).forEach((value, name) =>
-              console.log(`${name}: ${value}`),
-            );
+            // new FormData(e.currentTarget).forEach((value, name) =>
+            //   console.log(`${name}: ${value}`),
+            // );
 
+            console.log(`TEST`);
             e.currentTarget.reset();
           });
       })();
+// {
+//   () => {
+//     document
+//       .querySelector('.js-speaker-form')
+//       .addEventListener('submit', e => {
+//         e.preventDefault();
+
+//         new FormData(e.currentTarget).forEach((value, name) =>
+//         console.log())
+//     })
+// }
+  
+//   }
